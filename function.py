1ed3a88bcc173306303ae7e9954dc357025d3d20
Rene Cortez <rcortezk9@gmail.com>
2019-07-24 20:48:43 -0500

    Intro to numpy and classes

--- function.py
+++ function.py
@@ -1,7 +1,8 @@
 # Create function that returns the average of an integer list
 def average_numbers(num_list):
-    avg = sum(num_list)/float(len(num_list)) # divide by length of list
+    avg = sum(num_list) / float(len(num_list))  # divide by length of list
     return avg
+
 
 # Take the average of a list: my_avg
 my_avg = average_numbers([1, 2, 3, 4, 5, 6])
---
+++ intro_NumPy.py
@@ -0,0 +1,17 @@
+'''Create a function that returns a NumPy array'''
+
+# Import numpy as np
+import numpy as np
+
+# List input: my_matrix
+my_matrix = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+
+# Function that converts lists to arrays: return_array
+def return_array(matrix):
+    array = np.array(matrix, dtype=float)
+    return array
+
+
+# Call return_array on my_matrix, and print the output
+print(return_array(my_matrix))
